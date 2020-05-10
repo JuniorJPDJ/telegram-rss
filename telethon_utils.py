db0aff4896d94818861db802e33235fc9123f733
JuniorJPDJ <git@juniorjpdj.pl>
2020-05-10 20:15:16 +0200

    A bit better workaround in telethon sync hooks

--- telethon_utils.py
+++ telethon_utils.py
@@ -3,33 +3,33 @@ from argparse import ArgumentParser, Namespace
 import shlex
 
 from telethon.tl.types import MessageEntityBotCommand
-from telethon import TelegramClient
+import telethon
 
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Optional, IO, Text, NoReturn, Sequence, Union
-    from telethon import events
+    from typing import Optional, IO, Text, NoReturn, Union
+    from telethon import events, types
 
-# Yes, I know how it looks like, but I couldn't find another way to get own username in sync function
-# Still waiting for this issue to resolve this problem:
-# https://github.com/LonamiWebs/Telethon/issues/1344
-# <sync user getting hooks>
-getme = TelegramClient.get_me
-async def newgetme(self, input_peer=False):
-    ret = await getme(self, input_peer)
-    if not input_peer:
-        self.me = ret
-    return ret
-TelegramClient.get_me = newgetme
 
-start = TelegramClient._start
-async def newstart(self, phone, password, bot_token, force_sms, code_callback, first_name, last_name, max_attempts):
-    ret = await start(self, phone, password, bot_token, force_sms, code_callback, first_name, last_name, max_attempts)
-    await self.get_me()
-    return ret
-TelegramClient._start = newstart
-# </sync user getting hooks>
+class TelegramClient(telethon.TelegramClient):
+    # A bit better than before, but still workaround
+    # Still waiting for this issue to resolve this problem:
+    # https://github.com/LonamiWebs/Telethon/issues/1344
+    def __init__(self, *args, **kwargs):
+        super(TelegramClient, self).__init__(*args, **kwargs)
+        self.me = None
+
+    async def get_me(self, input_peer: bool = False) -> 'Union[types.User, types.InputPeerUser]':
+        me = await super(TelegramClient, self).get_me(input_peer)
+        if not input_peer:
+            self.me = me
+        return me
+
+    async def _start(self, *args, **kwargs):
+        ret = await super(TelegramClient, self)._start(*args, **kwargs)
+        await self.get_me()
+        return ret
 
 
 class ArgParserExit(Exception):
@@ -40,11 +40,6 @@ class ArgParser(ArgumentParser):
     def __init__(self, *args, **kwargs):
         super(ArgParser, self).__init__(*args, **kwargs)
         self._messages = []
-
-    # def parse_args(self, args: 'Optional[Union[Sequence[Text], Text]]' = ...) -> Namespace:
-    #     if isinstance(args, str):
-    #         args = shlex.split(args)
-    #     return super(ArgParser, self).parse_args(args)
 
     @staticmethod
     def convert_arg_line_to_args(arg_line: 'Text'):
@@ -85,14 +80,13 @@ def command(cmd: str, argparser: 'Optional[ArgParser]' = None):
 
     def find_command(ev: 'events.NewMessage.Event'):
         me = ev.client.me
-        username = '' if me is None else me.username
+        username = None if me is None else me.username
 
         for ent, txt in ev.message.get_entities_text():
             if isinstance(ent, MessageEntityBotCommand) and ent.offset == 0 and \
-              (txt == cmd or (txt.endswith(f'@{username}') and txt[:-len(username) - 1] == cmd)):
+              (txt == cmd or (username is not None and txt.endswith(f'@{username}') and txt[:-len(username) - 1] == cmd)):
                 ev.cmd = ent
-                # workaround for fail message
-                argparser.prog = txt
+                argparser.prog = txt        # command name setting in response messages
                 args = argparser.convert_arg_line_to_args(ev.message.message[len(txt)+1:])
                 try:
                     ev.args = argparser.parse_args(args)
