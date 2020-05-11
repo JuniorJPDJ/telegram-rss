e18c00af98fefa7a47968366ccb9129f64f38ad6
JuniorJPDJ <git@juniorjpdj.pl>
2020-05-11 02:47:47 +0200

    More utils and config rework

--- utils.py
+++ utils.py
@@ -1,14 +1,15 @@
-import asyncio
-from argparse import ArgumentParser, Namespace
+import argparse
 import shlex
 
+import durations_nlp
 from telethon.tl.types import MessageEntityBotCommand
 import telethon
+import yaml
 
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Optional, IO, Text, NoReturn, Union
+    from typing import Optional, IO, Text, NoReturn, Union, Callable, ClassVar, Generator, List
     from telethon import events, types
 
 
@@ -32,11 +33,16 @@ class TelegramClient(telethon.TelegramClient):
         return ret
 
 
+class Namespace(dict):
+    def __getattr__(self, *args, **kwargs):
+        return self.__getitem__(*args, **kwargs)
+
+
 class ArgParserExit(Exception):
     pass
 
 
-class ArgParser(ArgumentParser):
+class ArgParser(argparse.ArgumentParser):
     def __init__(self, *args, **kwargs):
         super(ArgParser, self).__init__(*args, **kwargs)
         self._messages = []
@@ -63,6 +69,7 @@ class ArgParser(ArgumentParser):
 
 
 def command(cmd: str, argparser: 'Optional[ArgParser]' = None):
+    # TODO: User/Group/Channel tagging as an argument
     """
     Parses command `cmd` from Telegram message using `argparser`
 
@@ -98,3 +105,43 @@ def command(cmd: str, argparser: 'Optional[ArgParser]' = None):
         return False
 
     return find_command
+
+
+def parse_time_interval(time_string):
+    # TODO: better time parsing
+    # https://github.com/scrapinghub/dateparser/issues/669
+    # dt = dateparser.parse(time_string, settings={'PARSERS': ['relative-time']})
+    # return abs(int((datetime.datetime.now() - dt).total_seconds())) if dt is not None else None
+    return durations_nlp.Duration(time_string).to_seconds()
+
+
+def yaml_constructor(tag: str, loader=yaml.SafeLoader):
+    def reg(func):
+        loader.add_constructor(tag, func)
+        return func
+    return reg
+
+
+class YAMLRemapper(object):
+    # You shouldn't even ask xDD
+    def __init__(self, loader=yaml.SafeLoader):
+        self.loader = loader
+        self.yaml_remappers: List[Callable[[yaml.Event], Generator[ClassVar[yaml.Event], None, None]]] = []
+
+    def yaml_remapper(self):
+        def dec(func: 'Callable[[Generator[yaml.Event, None, None]], Generator[yaml.Event, None, None]]'):
+            self.yaml_remappers.append(func)
+            return func
+        return dec
+
+    def parse(self, stream):
+        event_gen = yaml.parse(stream, self.loader)
+        for remapper in self.yaml_remappers:
+            event_gen = remapper(event_gen)
+        yield from event_gen
+
+    def load(self, stream):
+        # Yes.. I know.. I've not found how to deserialize events
+        yaml_events = self.parse(stream)
+        yaml_unparsed = yaml.emit(yaml_events)
+        return yaml.load(yaml_unparsed, self.loader)
---
+++ yamlconfig.py
@@ -0,0 +1,51 @@
+import os
+
+import yaml
+import yaml.resolver
+
+from utils import yaml_constructor, Namespace, parse_time_interval, YAMLRemapper
+
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Generator
+
+
+@yaml_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG)
+def namespace_constructor(loader, node):
+    loader.flatten_mapping(node)
+    return Namespace(loader.construct_pairs(node))
+
+
+@yaml_constructor('!time')
+def time_constructor(loader, node):
+    return parse_time_interval(loader.construct_scalar(node))
+
+
+@yaml_constructor("!env")
+def env_constructor(loader, node):
+    return os.environ.get(loader.construct_scalar(node), None)
+
+
+# handling of env var YAML in YAML
+yamlremapper = YAMLRemapper()
+
+
+@yamlremapper.yaml_remapper()
+def yamlenv_remapper(events: 'Generator[yaml.Event, None, None]') -> 'Generator[yaml.Event, None, None]':
+    for event in events:
+        if isinstance(event, yaml.ScalarEvent) and event.tag == '!yamlenv':
+            data = os.environ.get(event.value, "")
+            data = data if data else "sOmErAnDomStRiNg:"
+
+            started = False
+            for internal_event in yamlremapper.parse(data):
+                if isinstance(internal_event, yaml.DocumentEndEvent):
+                    break
+                elif started:
+                    yield internal_event
+                elif isinstance(internal_event, yaml.DocumentStartEvent):
+                    started = True
+        else:
+            yield event
